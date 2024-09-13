a81b0974048d08b1e952976a159a16a8408aa63f
rin-codingal <nurinn.codingal@gmail.com>
2024-09-13 19:49:21 +0700

    M17_L98

---
+++ 6-8/M-17/M17_L98_ACP.py
@@ -0,0 +1,25 @@
+f1 = input("what is your name? ")
+d1 = input("when is your birthday? ")
+print()
+
+f2 = input("what is your name? ")
+d2 = input("when is your birthday? ")
+print()
+
+f3 = input("what is your name? ")
+d3 = input("when is your birthday? ")
+print()
+
+f4 = input("what is your name? ")
+d4 = input("when is your birthday? ")
+print()
+
+f5 = input("what is your name? ")
+d5 = input("when is your birthday? ")
+print()
+
+print(f"{f1}, your birthday is {d1}")
+print(f"{f2}, your birthday is {d2}")
+print(f"{f3}, your birthday is {d3}")
+print(f"{f4}, your birthday is {d4}")
+print(f"{f5}, your birthday is {d5}")
