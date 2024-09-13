d482ae7b085e93ec741d5a6e7dac2442343019cb
rin-codingal <nurinn.codingal@gmail.com>
2024-09-14 06:55:42 +0700

    M17_L99

---
+++ 6-8/M-17/M17_L99_Act04.py
@@ -0,0 +1,11 @@
+# Take two strings as input from user
+print("Enter the First String: ")
+str1 = input()
+
+print("Enter the Second String: ")
+str2 = input()
+
+# Concatenate the two strings
+# Using Addition Operator
+str3 = str1 + str2
+print("\nConcatenated String: ", str3)
