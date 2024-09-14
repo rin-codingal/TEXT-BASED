1f1655a4b532db9a6dd3d0dcc67008034ae70180
rin-codingal <nurinn.codingal@gmail.com>
2024-09-14 07:23:30 +0700

    M17_L100

---
+++ 6-8/M-17/M17_L100_Act03.py
@@ -0,0 +1,13 @@
+# take marks as input from user
+print("Enter Marks Obtained in 3 Subjects: ")
+math = int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+
+# Let's calculate the percentage of marks
+sum = math+english+science
+print("sum of math, english, and science")
+
+perc = (sum/3400)*100
+
+print(f"Percentage Mark = {perc}")
