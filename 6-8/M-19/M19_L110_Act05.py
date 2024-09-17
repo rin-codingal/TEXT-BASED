0873b9f3ec6a292cadac95031ee69bdb13476207
rin-codingal <nurinn.codingal@gmail.com>
2024-09-18 05:31:05 +0700

    M19_L109-111

---
+++ 6-8/M-19/M19_L110_Act05.py
@@ -0,0 +1,15 @@
+def employee(name):
+  print(name)
+
+def salary(exp):
+  if exp>=5:
+    return 3000000
+  elif exp>=3:
+    return 1000000
+  else:
+    return 500000
+  
+employee("Rain")
+a = salary(15)
+
+print("The salary of the employee is ",a)
---
+++ 9-12/M08/M08_L43_Act01.py
@@ -0,0 +1,7 @@
+class sales:
+    def __init__(self,id):
+        self.id = id
+        id=100
+
+val=sales(123)
+print(val.id)
