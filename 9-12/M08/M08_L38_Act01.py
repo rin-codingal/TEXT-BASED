c88a17108ae4c158d9aad3c5e0a8e92b3ee4fdbe
rin-codingal <nurinn.codingal@gmail.com>
2024-09-17 15:27:07 +0700

    L38

---
+++ 9-12/M08/M08_L38_Act01.py
@@ -0,0 +1,30 @@
+name = "Raya"
+age = 17
+is_student = True
+weight = 43.7
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print()
+
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print()
+
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+print()
+
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+print()
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+print()
+
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
