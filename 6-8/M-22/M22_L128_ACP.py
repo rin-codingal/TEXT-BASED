1b9dd3f04a453395345324b0b0b5d5cee30d9ab7
rin-codingal <nurinn.codingal@gmail.com>
2024-09-18 21:50:51 +0700

    M22

---
+++ 6-8/M-22/M22_L128_ACP.py
@@ -0,0 +1,23 @@
+import pygame  
+
+pygame.init()  
+screen = pygame.display.set_mode((640, 480))  
+done = False  
+  
+#load the fonts  
+font = pygame.font.SysFont("Courier", 36)  
+# Render the text in new surface  
+text = font.render("My first Game Screen", True, (255, 16, 100))  
+
+while not done:  
+
+	for event in pygame.event.get():  
+		if event.type == pygame.QUIT:  
+			done = True  
+
+	screen.fill((255, 255, 255))  
+
+	#We will discuss blit() in the next topic  
+	screen.blit(text,(320 - text.get_width() // 2, 240 - text.get_height() // 2))  
+	pygame.draw.rect(screen, (28,171,226), pygame.Rect(30, 30, 60, 60))
+	pygame.display.flip()  
