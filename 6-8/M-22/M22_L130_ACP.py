21da5b7f54607e58d8174a6c58e39243e778f537
rin-codingal <nurinn.codingal@gmail.com>
2024-09-19 12:35:59 +0700

    M22_L130-132

--- 6-8/M-22/M22_L130_ACP.py
+++ 6-8/M-22/M22_L130_ACP.py
@@ -1,9 +1,21 @@
 import pygame
 import random
   
-surf_color = (0, 142, 204)
-color = (0, 0, 0)
-color2 = (255,0,0)
+surf_color = (0, 142, 204) #background color
+
+color1s1 = (150, 0, 150) #color 1 for sprite 1
+color2s1 = (0,0,255) #color 2 for sprite 1
+
+color1s2 = (0, 255, 0) #color 1 for sprite 2
+color2s2 = (255,0,0) #color 2 for sprite 2
+
+#position sprite 1
+pos1x = random.randint(0,480)
+pos1y = random.randint(0,370)
+
+#position sprite 2
+pos2x = random.randint(50,350)
+pos2y = random.randint(50,300)
 
 # Object class
 class Sprite(pygame.sprite.Sprite):
@@ -19,25 +31,18 @@ pygame.init()
 
 all_sprites_list = pygame.sprite.Group()
 
-# Add a sprite
-sp1 = Sprite(color, 20, 30)
-sp1.rect.x = random.randint(0,480)
-sp1.rect.y = random.randint(0,370)
-all_sprites_list.add(sp1)
+# sprite 1 color
+s1_active_color = color1s1
+s1_new_color = color2s1
 
-#second sprite
-sp2 = Sprite(color2, 20, 30)
-sp2.rect.x = random.randint(50,350)
-sp2.rect.y = random.randint(50,300)
-all_sprites_list.add(sp2)
+#sprite 2 color
+s2_active_color = color1s2
+s2_new_color = color2s2
 
 # ---- new code ----
 screen = pygame.display.set_mode((500, 400))
-pygame.display.set_caption("Creating Sprite")
+pygame.display.set_caption("Sprite Changing Color")
 screen.fill(surf_color)
-
-new_color = (0,255,0)
-bg_active_color = surf_color
 
 # custom user event to change color
 CHANGE_COLOR = pygame.USEREVENT + 1
@@ -58,12 +63,39 @@ while exit:
 
 		# ---- new code ----
 		if event.type == CHANGE_COLOR:
-			if bg_active_color == new_color:
-				sp1.fill(new_color)
-				bg_active_color = surf_color
-			elif bg_active_color == surf_color:
-				screen.fill(surf_color)
-				bg_active_color = new_color
+			#sprite 1 changing color
+			if s1_active_color == s1_new_color :
+				sp1 = Sprite(s1_new_color, 20, 30)
+				sp1.rect.x = pos1x
+				sp1.rect.y = pos1y
+				all_sprites_list.add(sp1)
+
+				s1_active_color = color1s1
+
+			elif s1_active_color == color1s1 :
+				sp1 = Sprite(color1s1, 20, 30)
+				sp1.rect.x = pos1x
+				sp1.rect.y = pos1y
+				all_sprites_list.add(sp1)
+
+				s1_active_color = s1_new_color
+			
+			#sprite 2 changing color
+			if s2_active_color == s2_new_color :
+				sp2 = Sprite(s2_new_color, 20, 30)
+				sp2.rect.x = pos2x
+				sp2.rect.y = pos2y
+				all_sprites_list.add(sp2)
+
+				s2_active_color = color1s2
+			elif s2_active_color == color1s2 :
+				sp2 = Sprite(color1s2, 20, 30)
+				sp2.rect.x = pos2x
+				sp2.rect.y = pos2y
+				all_sprites_list.add(sp2)
+
+				s2_active_color = s2_new_color
+			
   
 	all_sprites_list.update()
 	all_sprites_list.draw(screen)
