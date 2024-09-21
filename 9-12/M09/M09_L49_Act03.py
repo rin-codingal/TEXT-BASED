4fceac816e0dedef27a77a509d56a094a0dd4b33
rin-codingal <nurinn.codingal@gmail.com>
2024-09-21 07:29:45 +0700

    M09_L49

---
+++ 9-12/M09/M09_L49_Act03.py
@@ -0,0 +1,17 @@
+# Program to count number of lines in this file
+# Opening a file
+file = open("test2.txt","r")
+Counter = 0
+
+# Reading from file
+content = file.read()
+
+# splitting content into lines
+# and storing them in a list
+content_list = content.split("\n")
+
+for i in content_list:
+	if i:
+		Counter += 1
+		
+print("This is the number of lines in the file:", Counter)
