3c896ee41e6d3dce5e1830db3607e284b4e9dc34
rin-codingal <nurinn.codingal@gmail.com>
2024-10-11 06:55:14 +0700

    M24

---
+++ 6-8/M-24/M24_L144_Act01.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv(r"C:\Users\nurin\Documents\CODINGAL\CLASS\PAID\TEXT-BASED\6-8\M-24\penguins_size.csv")
+
