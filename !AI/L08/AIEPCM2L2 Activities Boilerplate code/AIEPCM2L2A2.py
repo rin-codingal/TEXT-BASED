1582c374e28b03f142f69be2841042a97110abb5
rin-codingal <nurinn.codingal@gmail.com>
2025-03-13 22:36:04 +0700

    noai
    	deleted:    !AI/AI_L01.py
    	deleted:    !AI/L02/L02-template.py
    	deleted:    !AI/L02/sentimentspy.py
    	deleted:    !AI/L03/L03-template.py
    	deleted:    !AI/L03/rule-based-chatbot.py
    	deleted:    !AI/L04 - tictactoe.py
    	deleted:    !AI/L05 - Simple digit predictor.py
    	deleted:    !AI/L06 - AI Movie Recommendation.py
    	deleted:    !AI/L06 - template.py
    	deleted:    !AI/L07/Sidd hw.py
    	deleted:    !AI/L07/a1.py
    	deleted:    !AI/L07/a2.py
    	deleted:    !AI/L07/example.jpg
    	deleted:    !AI/L08/AIEPCM2L2 Activities Boilerplate code/AIEPCM2L2A1.py
    	deleted:    !AI/L08/AIEPCM2L2 Activities Boilerplate code/AIEPCM2L2A2.py
    	deleted:    !AI/L08/AIEPCM2L2 Activities Boilerplate code/example.jpg
    	deleted:    !AI/L08/AIEPCM2L2A1.py
    	deleted:    !AI/L08/AIEPCM2L2A2.py
    	deleted:    !AI/L08/AIEPCM2L2_Activities-72a4.zip
    	deleted:    !AI/L08/example2.jpg
    	deleted:    !AI/L08/sample color.html
    	deleted:    !AI/L08/sample color2.html
    	deleted:    !AI/L09/Image_annotation-template.py
    	deleted:    !AI/L09/Image_annotation.py
    	deleted:    !AI/L09/Sidd HW.py
    	deleted:    !AI/L09/editedImage.jpg
    	deleted:    !AI/L09/editedImage2.jpg
    	deleted:    !AI/L09/example.jpg
    	deleted:    !AI/L10/example.jpg
    	deleted:    !AI/L10/interactive edge detection and filtering.py
    	deleted:    !AI/L11/color filters on static image.py
    	deleted:    !AI/L11/example.jpg
    	deleted:    !AI/L12/face detection.py
    	deleted:    !AI/L13/face tracking and counting people.py
    	deleted:    !AI/L14/example.jpg
    	deleted:    !AI/L14/real time color filter and edge detection.py
    	deleted:    !AI/L15/realtime gesture control.py
    	deleted:    !AI/L16/gesture based volume and brightmess control.py
    	deleted:    !AI/L17/scrolling using hand gesture.py
    	deleted:    !AI/L18/gesture-controlled photo app.py
    	deleted:    !AI/L19/fetch data from public APIs.py
    	deleted:    !AI/L20/fetch and display trivia questions.py
    	deleted:    !AI/L21/random facts.py
    	deleted:    !AI/Sidd HW RPS.py
    	deleted:    !AI/imdb_top_1000.csv

--- !AI/L08/AIEPCM2L2 Activities Boilerplate code/AIEPCM2L2A2.py
+++
@@ -1,7 +0,0 @@
-#  Rotating and Adjusting Brightness
-
-# Rotate the image by 45 degrees around its center
-  # rotate by 45 degrees
-
-# Increase brightness by adding 50 to all pixel values
-# Use cv2.add to avoid negative values or overflow
